ab68b147b383259ce299c98ab27d0c4f0b440e9d
Jason Li <li07.jason@gmail.com>
2016-09-01 00:41:36 -0400

    started trying out WPF

---
+++ Tests/WPFTestApp/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jason
+ * Date: 9/1/2016
+ * Time: 12:01 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WPFTestApp
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		int count;
+		
+		public Window1()
+		{
+			InitializeComponent();
+			this.count = 0;
+			this.Counter.Content = count;
+		}
+		
+		void Increment_Click(object sender, RoutedEventArgs e)
+		{
+			++this.count;
+			this.Counter.Content = count;
+		}
+		void ResetCount_Click(object sender, RoutedEventArgs e)
+		{
+			this.count = 0;
+			this.Counter.Content = count;
+		}
+	}
+}
